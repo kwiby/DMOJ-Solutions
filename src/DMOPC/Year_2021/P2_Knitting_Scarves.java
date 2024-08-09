6ccf4e381cea9e77d37dc6971e75cf9148f462f3
kwiby <134721335+kwiby@users.noreply.github.com>
2024-08-09 14:59:26 -0400

    DMOPC '21 Contest 7 P2 - Knitting Scarves {SOLVED}

--- src/DMOPC/Year_2021/P2_Knitting_Scarves.java
+++ src/DMOPC/Year_2021/P2_Knitting_Scarves.java
@@ -1,10 +1,9 @@
-package DMOPC.Year_2021; // Create CUSTOM (not inbuilt one) Doubly-Linked List!
+package DMOPC.Year_2021;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class P2_Knitting_Scarves {
     public static void main(String[] args) throws IOException {
@@ -12,39 +11,40 @@ public class P2_Knitting_Scarves {
         StringTokenizer st = new StringTokenizer(user.readLine());
 
         int N = Integer.parseInt(st.nextToken());
-        LinkedList<Integer> list = new LinkedList<>();
-        for (int i = 1; i <= N; i++) {
-            list.add(i);
+        int Q = Integer.parseInt(st.nextToken());
+
+        int[] prev = new int[N + 2]; // Previous node.
+        int[] next = new int[N + 2]; // Next node.
+
+        for (int i = 0; i < N + 1; i++) {
+            prev[i] = i - 1;
+            next[i] = i + 1;
         }
 
-        int Q = Integer.parseInt(st.nextToken());
         for (int i = 0; i < Q; i++) {
             st = new StringTokenizer(user.readLine());
             int L = Integer.parseInt(st.nextToken());
             int R = Integer.parseInt(st.nextToken());
             int K = Integer.parseInt(st.nextToken());
 
-            int iL = list.indexOf(L);
-            int iR = list.indexOf(R);
-            int iK = list.indexOf(K);
+            if (prev[L] != K) { // Making sure it doesn't try adding L -> R to the same spots and wasting time.
+                int l = prev[L];
+                int r = next[R];
+                int k = next[K];
 
-            if (iK < iL) {
-                int count = 1;
-                for (int j = iL; j <= iR; j++) {
-                    int temp = list.remove(j);
-                    list.add(iK + count, temp);
-                    count++;
-                }
-            } else {
-                for (int j = 0; j <= iR - iL; j++) {
-                    int temp = list.remove(iL);
-                    list.add(iK, temp);
-                }
+                prev[L] = K;
+                next[R] = k;
+                next[K] = L;
+                next[l] = r;
+                prev[r] = l;
+                prev[k] = R;
             }
         }
 
-        for (int x : list) {
-            System.out.print(x + " ");
+        int colour = 0;
+        while (next[colour] != N + 1) {
+            colour = next[colour];
+            System.out.print(colour + " ");
         }
     }
 }
