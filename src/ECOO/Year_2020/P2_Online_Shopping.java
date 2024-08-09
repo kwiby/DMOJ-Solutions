7865e5819fd8c71d6363558c27a07195f17725e8
kwiby <134721335+kwiby@users.noreply.github.com>
2024-08-09 13:04:48 -0400

    ECOO '20 P2 - Online Shopping {SOLVED}

--- src/ECOO/Year_2020/P2_Online_Shopping.java
+++ src/ECOO/Year_2020/P2_Online_Shopping.java
@@ -13,10 +13,10 @@ public class P2_Online_Shopping {
         BufferedReader user = new BufferedReader(new InputStreamReader(System.in));
 
         int testCases = Integer.parseInt(user.readLine());
-        int numOfStores = Integer.parseInt(user.readLine());
 
         for (int i = 0; i < testCases; i++) {
             Map<String, TreeMap<Integer, Integer>> itemInfo = new HashMap<>();
+            int numOfStores = Integer.parseInt(user.readLine());
 
             for (int j = 0; j < numOfStores; j++) {
                 int numOfItems = Integer.parseInt(user.readLine());
@@ -26,17 +26,15 @@ public class P2_Online_Shopping {
                     String itemName = st.nextToken();
                     int itemPrice = Integer.parseInt(st.nextToken());
                     int itemQuantity = Integer.parseInt(st.nextToken());
-                    System.out.println("\n" + itemName + " " + itemPrice + " " + itemQuantity);
 
-                    if (!itemInfo.containsKey(itemName)) {
+                    if (!itemInfo.containsKey(itemName)) { // If there is already an item with the same name in the map.
                         TreeMap<Integer, Integer> itemDetails = new TreeMap<>();
                         itemDetails.put(itemPrice, itemQuantity);
                         itemInfo.put(itemName, itemDetails);
                     } else {
-                        if (itemInfo.get(itemName).containsKey(itemPrice)) {
-                            TreeMap<Integer, Integer> itemDetails = itemInfo.get(itemName);
-                            itemDetails.put(itemPrice, itemDetails.get(itemPrice) + itemQuantity);
-                        } else {
+                        if (itemInfo.get(itemName).containsKey(itemPrice)) { // If there is already an item with the same price in the treemap.
+                            itemInfo.get(itemName).put(itemPrice, itemInfo.get(itemName).get(itemPrice) + itemQuantity);
+                        } else { // If there isn't already an item with the same price in the treemap.
                             itemInfo.get(itemName).put(itemPrice, itemQuantity);
                         }
                     }
@@ -54,13 +52,11 @@ public class P2_Online_Shopping {
 
                 while (numToBuy > 0) {
                     TreeMap<Integer, Integer> itemDetails = itemInfo.get(itemName);
-                    System.out.println("\nitemDetails = " + itemDetails);
-                    System.out.println("numToBuy = " + numToBuy);
 
-                    if (numToBuy >= itemDetails.firstEntry().getValue()) {
+                    if (numToBuy >= itemDetails.firstEntry().getValue()) { // If the # of items to buy is greater than or equal to the quantity of the item at a specific price.
                         minCost += itemDetails.firstEntry().getKey() * itemDetails.firstEntry().getValue();
                         numToBuy -= itemDetails.pollFirstEntry().getValue();
-                    } else {
+                    } else { // If the # of items to buy is less than the quantity of the item at a specific price.
                         minCost += itemDetails.firstEntry().getKey() * numToBuy;
                         itemDetails.put(itemDetails.firstEntry().getKey(), itemDetails.get(itemDetails.firstEntry().getKey()) - numToBuy);
                         numToBuy = 0;
